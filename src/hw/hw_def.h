0d4c8c8cb27ea1bdae554a1b87379847aa208581
era <73109780+eerraa@users.noreply.github.com>
2025-10-22 20:03:51 +0900

    V251011R3: guard coarse step 보정으로 잔차 수렴 개선

--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -6,7 +6,7 @@
 #include QMK_KEYMAP_CONFIG_H
 
 
-#define _DEF_FIRMWATRE_VERSION      "V251010R8"  // V251010R8: usbhid rate 출력 재정비 및 통계 보정
+#define _DEF_FIRMWATRE_VERSION      "V251011R3"  // V251011R3: guard 초과 시 coarse step 보정으로 잔차 수렴 가속
 #define _DEF_BOARD_NAME             "BARAM-QMK-H7S-FW"
 
 
