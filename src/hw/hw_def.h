0362210517b853acdde5f6477a28bd51bdd6809b
era <73109780+eerraa@users.noreply.github.com>
2025-10-26 17:58:04 +0900

    V251011R7: HID 타이머 전송 대기열 도입

--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -6,7 +6,7 @@
 #include QMK_KEYMAP_CONFIG_H
 
 
-#define _DEF_FIRMWATRE_VERSION      "V251010R8"  // V251010R8: usbhid rate 출력 재정비 및 통계 보정
+#define _DEF_FIRMWATRE_VERSION      "V251011R7"  // V251011R7: 전송 대기열 도입으로 잔차 샘플 매핑 안정화
 #define _DEF_BOARD_NAME             "BARAM-QMK-H7S-FW"
 
 
