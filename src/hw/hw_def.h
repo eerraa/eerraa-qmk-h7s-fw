6eef7bb8fafa2568e07357646f920267142ab8bb
era <73109780+eerraa@users.noreply.github.com>
2025-10-29 09:29:50 +0900

    V251012R5: rgblight 인디케이터 반복 연산 최적화

--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -6,7 +6,7 @@
 #include QMK_KEYMAP_CONFIG_H
 
 
-#define _DEF_FIRMWATRE_VERSION      "V251012R4"  // V251012R4: Brick60 rgblight 인디케이터 파이프라인 추가 리팩토링
+#define _DEF_FIRMWATRE_VERSION      "V251012R5"  // V251012R5: rgblight 인디케이터 중복 연산 방지 최적화
 #define _DEF_BOARD_NAME             "BARAM-QMK-H7S-FW"
 
 
