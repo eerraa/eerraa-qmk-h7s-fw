99fd79d3164627f0572a748e73d9338f6002485a
era <73109780+eerraa@users.noreply.github.com>
2025-11-04 01:17:30 +0900

    V251016R9: 인디케이터 프레임 준비 경로 단순화

--- src/hw/hw_def.h
+++ src/hw/hw_def.h
@@ -6,7 +6,7 @@
 #include QMK_KEYMAP_CONFIG_H
 
 
-#define _DEF_FIRMWATRE_VERSION      "V251016R8"   // V251016R8: 인디케이터 범위 전달 구조 정비 및 오버레이 대응
+#define _DEF_FIRMWATRE_VERSION      "V251016R9"   // V251016R9: 인디케이터 프레임 준비 로직 단순화
 #define _DEF_BOARD_NAME             "BARAM-QMK-H7S-FW"
 
 
